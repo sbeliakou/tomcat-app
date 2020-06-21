622982b7bd574f931f4edfa4870f4fac59ce1980
Siarhei Beliakou <sbeliakou@gmail.com>
2020-06-21 03:36:29 +0300

    --rm

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it -v $(pwd):$(pwd) -w $(pwd) maven:3.6-jdk-8-alpine \
+docker run -it --rm -v $(pwd):$(pwd) -w $(pwd) maven:3.6-jdk-8-alpine \
   bash -c 'mvn -e -B dependency:resolve; mvn -e -B package'
